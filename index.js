8df995c1d4612f58757e7d64cc3441f1cbee3ba7
Calin Tamas <calinntamas@gmail.com>
2019-10-25 10:33:24 +0300

    fix export

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import Toast from './src'
 import Controller from './src/controller'
 
-const ToastController = Controller;
-
-export ToastController
+export const ToastController = Controller;
 
 export default Toast
