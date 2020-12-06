eaae137dacc7c41a55daca86ce252eb5fcd2b8a9
Calin Tamas <calinntamas@gmail.com>
2020-12-06 11:58:45 +0200

    cleanup base toast styles

--- src/components/error.js
+++ src/components/error.js
@@ -4,8 +4,16 @@ import BaseToast from './base';
 import { icons } from '../assets';
 import colors from '../colors';
 
-const ErrorToast = (props) => {
-  return <BaseToast {...props} color={colors.blazeOrange} icon={icons.error} />;
-};
+function ErrorToast(props) {
+  return (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: colors.blazeOrange }}
+      leadingIcon={icons.error}
+    />
+  );
+}
+
+ErrorToast.propTypes = BaseToast.propTypes;
 
 export default ErrorToast;
