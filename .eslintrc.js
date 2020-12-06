fde0845963525a6ac7a0708d3306016997ccd07e
Calin Tamas <calinntamas@gmail.com>
2020-12-06 12:57:15 +0200

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
   parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier'],
-  plugins: ['react-hooks'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react-hooks', 'prettier'],
   globals: {
     __DEV__: false,
     fetch: false
@@ -10,18 +10,15 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': 0,
     'global-require': 0,
-    'react/prop-types': 0,
+    'react/prop-types': 'warn',
     'react/jsx-closing-bracket-location': 0,
     'react/jsx-props-no-spreading': 0,
-    'react/no-array-index-key': 0,
-    'react/sort-comp': 0,
     'react-hooks/rules-of-hooks': 2,
     'react-hooks/exhaustive-deps': 1,
-    'prefer-const': 0,
     'no-underscore-dangle': 0,
-    'no-plusplus': 0,
     'import/prefer-default-export': 0,
     'class-methods-use-this': 0,
-    'react/jsx-filename-extension': 0
+    'react/jsx-filename-extension': 0,
+    'react/sort-comp': 0
   }
 };
--- src/utils/obj.js
+++ src/utils/obj.js
@@ -1,10 +1,9 @@
-const includeKeys = ({ obj = {}, keys = [] }) => {
-  return Object.keys(obj).reduce((acc, key) => {
+const includeKeys = ({ obj = {}, keys = [] }) =>
+  Object.keys(obj).reduce((acc, key) => {
     if (keys.includes(key)) {
       acc[key] = obj[key];
     }
     return acc;
   }, {});
-};
 
 export { includeKeys };
