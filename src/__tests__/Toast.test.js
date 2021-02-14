fe374ee90dae345a71e12c13cdf9bd5006912b6c
Calin Tamas <calinntamas@gmail.com>
2021-02-14 18:26:51 +0200

    write behavior tests (#146)

--- src/__tests__/Toast.test.js
+++ src/__tests__/Toast.test.js
@@ -2,21 +2,16 @@
 
 import { act, fireEvent, render, waitFor } from '@testing-library/react-native';
 import React from 'react';
+import { View } from 'react-native';
 
 import Toast from '..';
 import colors from '../colors';
-
-const FRAME_TIME = 10;
-
-global.requestAnimationFrame = (cb) => {
-  setTimeout(cb, FRAME_TIME);
-};
 
 function setup(props) {
   const ref = {
     current: undefined
   };
-  const utils = render(<Toast ref={ref} {...props} />);
+  const utils = render(<Toast ref={ref} autoHide={false} {...props} />);
   const getAnimatedView = () => utils.queryByTestId('animatedView');
   const getText1 = () => utils.queryByTestId('text1');
   const getText2 = () => utils.queryByTestId('text2');
@@ -37,115 +32,200 @@ function getVerticalOffset(reactElement) {
 }
 
 describe('test Toast behavior', () => {
-  it('becomes visible when show() is called, hides when hide() is called', async () => {
-    const { ref, getText1, getText2, getAnimatedView } = setup();
-    let animatedView = getAnimatedView();
+  describe('test API', () => {
+    it('becomes visible when show() is called, hides when hide() is called', async () => {
+      const { ref, getText1, getText2, getAnimatedView } = setup();
+      let animatedView = getAnimatedView();
 
-    expect(getText1()).toBeFalsy();
-    // Toast View is pushed off screen, on the Y axis
-    expect(getVerticalOffset(animatedView) < 0).toBe(true);
+      expect(getText1()).toBeFalsy();
+      // Toast View is pushed off screen, on the Y axis
+      expect(getVerticalOffset(animatedView) < 0).toBe(true);
 
-    await act(async () => {
-      await ref.current.show({
-        text1: 'Hello',
-        text2: 'Test'
+      await act(async () => {
+        await ref.current.show({
+          text1: 'Hello',
+          text2: 'Test'
+        });
+      });
+      await waitFor(() => getText1());
+      await waitFor(() => getText2());
+      animatedView = getAnimatedView();
+      expect(getVerticalOffset(animatedView) < 0).toBe(false);
+
+      await act(async () => {
+        await ref.current.hide();
+      });
+      animatedView = getAnimatedView();
+      expect(getVerticalOffset(animatedView) < 0).toBe(true);
+    });
+
+    it('shows success type', async () => {
+      const { ref, getRootView } = setup();
+
+      await act(async () => {
+        await ref.current.show({
+          type: 'success'
+        });
+      });
+
+      expect(getRootView()).toHaveStyle({
+        borderLeftColor: colors.mantis
       });
     });
-    await waitFor(() => getText1());
-    await waitFor(() => getText2());
-    animatedView = getAnimatedView();
-    expect(getVerticalOffset(animatedView) < 0).toBe(false);
 
-    await act(async () => {
-      await ref.current.hide();
+    it('shows info type', async () => {
+      const { ref, getRootView } = setup();
+
+      await act(async () => {
+        await ref.current.show({
+          type: 'info'
+        });
+      });
+
+      expect(getRootView()).toHaveStyle({
+        borderLeftColor: colors.lightSkyBlue
+      });
     });
-    animatedView = getAnimatedView();
-    expect(getVerticalOffset(animatedView) < 0).toBe(true);
+
+    it('shows error type', async () => {
+      const { ref, getRootView } = setup();
+
+      await act(async () => {
+        await ref.current.show({
+          type: 'error'
+        });
+      });
+
+      expect(getRootView()).toHaveStyle({
+        borderLeftColor: colors.blazeOrange
+      });
+    });
+
+    it('calls onShow', async () => {
+      const onShow = jest.fn();
+      const { ref } = setup();
+
+      await act(async () => {
+        await ref.current.show({
+          type: 'info',
+          onShow
+        });
+      });
+
+      expect(onShow).toHaveBeenCalled();
+    });
+
+    it('calls onHide', async () => {
+      const onHide = jest.fn();
+      const { ref } = setup();
+
+      await act(async () => {
+        await ref.current.show({
+          type: 'info',
+          onHide
+        });
+        await ref.current.hide();
+      });
+
+      expect(onHide).toHaveBeenCalled();
+    });
+
+    it('fires onPress', async () => {
+      const onPress = jest.fn();
+      const { ref, getRootView } = setup();
+
+      await act(async () => {
+        await ref.current.show({
+          type: 'info',
+          onPress
+        });
+      });
+
+      fireEvent.press(getRootView());
+      expect(onPress).toHaveBeenCalled();
+    });
+
+    it('shows at the bottom', async () => {
+      const { ref, getAnimatedView } = setup();
+
+      await act(async () => {
+        await ref.current.show({
+          position: 'bottom'
+        });
+      });
+      expect(getAnimatedView()).toHaveStyle({
+        bottom: 0
+      });
+    });
+
+    it('shows with custom top offset', async () => {
+      const { ref, getAnimatedView } = setup();
+      const offset = 60;
+      const height = 65;
+
+      let animatedView = getAnimatedView();
+      expect(getVerticalOffset(animatedView)).toBe(-height);
+
+      await act(async () => {
+        await ref.current.show({
+          topOffset: offset
+        });
+      });
+
+      animatedView = getAnimatedView();
+      expect(getVerticalOffset(animatedView)).toBe(offset);
+    });
+
+    it('shows with custom bottom offset', async () => {
+      const { ref, getAnimatedView } = setup();
+      const offset = 60;
+      const height = 65;
+
+      let animatedView = getAnimatedView();
+      expect(getVerticalOffset(animatedView)).toBe(-height);
+
+      await act(async () => {
+        await ref.current.show({
+          position: 'bottom',
+          bottomOffset: offset
+        });
+      });
+
+      animatedView = getAnimatedView();
+      expect(getVerticalOffset(animatedView)).toBe(-offset);
+    });
   });
 
-  it('shows success type', async () => {
-    const { ref, getRootView } = setup();
-
-    await act(async () => {
-      await ref.current.show({
-        type: 'success'
+  describe('test props', () => {
+    it('shows Toast from custom config', async () => {
+      const testID = 'testView';
+      const { ref, queryByTestId } = setup({
+        config: {
+          test: () => <View testID={testID} />
+        }
       });
-    });
 
-    expect(getRootView()).toHaveStyle({
-      borderLeftColor: colors.mantis
-    });
-  });
-
-  it('shows info type', async () => {
-    const { ref, getRootView } = setup();
-
-    await act(async () => {
-      await ref.current.show({
-        type: 'info'
+      await act(async () => {
+        await ref.current.show({
+          type: 'test'
+        });
       });
+
+      const testView = queryByTestId(testID);
+      expect(testView).toBeTruthy();
     });
 
-    expect(getRootView()).toHaveStyle({
-      borderLeftColor: colors.lightSkyBlue
-    });
-  });
+    it('tries to show Toast type that does not exist', async () => {
+      const { ref, getRootView } = setup();
 
-  it('shows error type', async () => {
-    const { ref, getRootView } = setup();
-
-    await act(async () => {
-      await ref.current.show({
-        type: 'error'
+      await act(async () => {
+        await ref.current.show({
+          type: 'test'
+        });
       });
+
+      const rootView = getRootView();
+      expect(rootView).toBeFalsy();
     });
-
-    expect(getRootView()).toHaveStyle({
-      borderLeftColor: colors.blazeOrange
-    });
-  });
-
-  it('calls onShow', async () => {
-    const onShow = jest.fn();
-    const { ref } = setup();
-
-    await act(async () => {
-      await ref.current.show({
-        type: 'info',
-        onShow
-      });
-    });
-
-    expect(onShow).toHaveBeenCalled();
-  });
-
-  it('calls onHide', async () => {
-    const onHide = jest.fn();
-    const { ref } = setup();
-
-    await act(async () => {
-      await ref.current.show({
-        type: 'info',
-        onHide
-      });
-      await ref.current.hide();
-    });
-
-    expect(onHide).toHaveBeenCalled();
-  });
-
-  it('fires onPress', async () => {
-    const onPress = jest.fn();
-    const { ref, getRootView } = setup();
-
-    await act(async () => {
-      await ref.current.show({
-        type: 'info',
-        onPress
-      });
-    });
-
-    fireEvent.press(getRootView());
-    expect(onPress).toHaveBeenCalled();
   });
 });
