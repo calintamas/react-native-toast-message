c6c7dacbe224280692ce0ebfbee6a4e5d7f51add
Calin Tamas <calinntamas@gmail.com>
2021-05-01 12:13:43 +0300

    use backpacker-react eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,8 @@
 module.exports = {
-  parser: 'babel-eslint',
-  root: true,
-  globals: {
-    __DEV__: false,
-    fetch: false
-  },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:react-native-a11y/basic',
-    'plugin:react-hooks/recommended'
-  ],
-  plugins: ['react-hooks', 'prettier'],
+  extends: ['backpacker-react'],
   rules: {
-    'react/jsx-filename-extension': 'off',
-    'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'no-underscore-dangle': 'off',
     'react/sort-comp': 'off',
-    'import/no-cycle': 'error',
-    'react/prop-types': ['error', { ignore: ['children'] }]
+    'no-underscore-dangle': 'warn'
   }
 };
