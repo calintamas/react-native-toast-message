bdb9fea0cb896d9dde5c3c596f632cb626d94f15
Hema Sama <mrlibya003@gmail.com>
2021-07-23 15:36:31 +0200

    export default toast

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import Toast from './src';
 
 export { default as BaseToast } from './src/components/base';
+export { default as SuccessToast } from './src/components/success';
+export { default as ErrorToast } from './src/components/error';
+export { default as InfoToast } from './src/components/info';
 export default Toast;
