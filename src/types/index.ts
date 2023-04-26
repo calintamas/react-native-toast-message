a44f0b6cd1bee218dc4a217af2f081184fee2b55
002huiguo <55090070+002huiguo@users.noreply.github.com>
2023-04-26 12:21:17 +0800

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,7 +10,7 @@ import {
 
 export type ReactChildren = React.ReactNode;
 
-export type ToastType = string;
+export type ToastType = 'success' | 'info' | 'error';
 export type ToastPosition = 'top' | 'bottom';
 
 export type ToastOptions = {
