97ca1e8b2b441f07ab6e7728f46d61ea1d154d41
Seyed Mostafa Hasani <seyedmostafahassani@gmail.com>
2023-12-15 14:00:54 +0330

    chore: update type location

--- src/hooks/useSafeArea.ts
+++ src/hooks/useSafeArea.ts
@@ -1,8 +1,25 @@
 import {NativeModules, StatusBar} from 'react-native'
 import {isIOS} from "../utils/platform";
 import {useEffect, useState} from "react";
-import {TSafeArea} from "../types";
 import {SCREEN_HEIGHT, WINDOW_HEIGHT} from "../utils/dimension";
+
+type TSafeArea = {
+	top: number
+	bottom: number
+	left: number
+	right: number
+}
+
+type TNativeModulesSafeArea = {
+	SafeAreaInsetsModule: {
+		getSafeAreaInsets(p: (error: Error, result: TSafeArea) => void): Promise<{
+			top: number;
+			bottom: number;
+			left: number;
+			right: number
+		}>;
+	};
+}
 export const useSafeArea = () => {
 	const [safeAreaInsets, setSafeAreaInsets] = useState<TSafeArea | null>(null);
 	useEffect(() => {
--- src/types/index.ts
+++ src/types/index.ts
@@ -200,21 +200,3 @@ export type ToastProps = {
    */
   onPress?: () => void;
 };
-
-export type TSafeArea = {
-  top: number
-  bottom: number
-  left: number
-  right: number
-}
-
-export type TNativeModulesSafeArea = {
-  SafeAreaInsetsModule: {
-    getSafeAreaInsets(p: (error: Error, result: TSafeArea) => void): Promise<{
-      top: number;
-      bottom: number;
-      left: number;
-      right: number
-    }>;
-  };
-}
